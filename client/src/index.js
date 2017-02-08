dd48be8f6a13c9067098104c13b8496cb7934b59
Emanuele <emanuele@lostcrew.it>
2017-02-08 22:56:15 +0100

    Add phoenix connection

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,33 @@
+import { Socket } from 'phoenix'
+import { default as paper, view, Path, Point } from 'paper'
 import './main.css'
-import { default as paper, Path, Point } from 'paper'
 
-const canvas = document.querySelector('[data-canvas]')
-let myPath;
+function initCanvas (channel) {
+  const canvas = document.querySelector('[data-canvas]')
+  let myPath;
 
-paper.setup(canvas)
-
-const path = new Path()
-path.strokeColor = 'black'
-
-paper.view.onMouseDown = (event) => {
-  myPath = new Path()
-  myPath.strokeColor = 'black'
+  paper.setup(canvas)
+  paper.view.onMouseDown = event => {
+    myPath = new Path()
+    myPath.strokeColor = 'black'
+  }
+  paper.view.onMouseDrag = event => {
+    myPath.add(event.point)
+    channel.push('point:updated', { ...event.point, color: 'black' })
+  }
 }
 
-paper.view.onMouseDrag = (event) => {
-  myPath.add(event.point)
-  console.log(event.point.x, event.point.y)
-}
+const params = { user: 1 }
+const socket = new Socket('ws://192.168.2.5:4000/socket', { params })
+
+socket.connect()
+const channel = socket.channel('draw:fake_uuid')
+channel.on('point:updated', payload => {
+  console.log(payload.body)
+})
+channel.join()
+  .receive('ok', res => {
+    console.log('Joined', res)
+    initCanvas(channel)
+  })
+  .receive('error', res => { console.log('Joined', res) })
