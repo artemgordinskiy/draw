3849f9df06709a333feb7443a792d15e00253e1f
Francesco Bonomi <frbonomi@gmail.com>
2017-02-08 22:16:13 +0100

    Canvas free drawing

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,20 @@
-console.log('frankieee')
+import './main.css'
+import { default as paper, Path, Point } from 'paper'
+
+const canvas = document.querySelector('[data-canvas]')
+let myPath;
+
+paper.setup(canvas)
+
+const path = new Path()
+path.strokeColor = 'black'
+
+paper.view.onMouseDown = (event) => {
+  myPath = new Path()
+  myPath.strokeColor = 'black'
+}
+
+paper.view.onMouseDrag = (event) => {
+  myPath.add(event.point)
+  console.log(event.point.x, event.point.y)
+}
