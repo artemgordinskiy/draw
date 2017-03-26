8021a23da9fae2188314cf73eb3b72c3691806b2
ArtemGordinsky <artem@gordinskiy.com>
2017-03-26 19:27:26 +0200

    Add a show_flash layout function I stole from somewhere

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,25 @@
 defmodule Draw.LayoutView do
   use Draw.Web, :view
+
+  defp render_flash(type, message) do
+    ~E"""
+    <div class="alert alert-<%= type %>" role="alert">
+      <%= message %>
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    """
+  end
+
+  def show_flash(conn) do
+    case get_flash(conn) do
+      %{"info" => msg} ->
+        render_flash("info", msg)
+      %{"error" => msg} ->
+        render_flash("danger", msg)
+      _ ->
+        ""
+    end
+  end
 end
