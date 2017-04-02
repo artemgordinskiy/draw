a6d92c6b5c0722dfeec9c5088f0abeb18780cb75
ArtemGordinsky <artem@gordinskiy.com>
2017-04-02 15:16:53 +0200

    Show last 10 drawings on the main page

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,7 +4,13 @@ defmodule Draw.PageController do
   alias Draw.Drawing
 
   def index(conn, _params) do
-    render conn, "index.html", changeset: Drawing.changeset(%Drawing{}),
-           action: "/new-drawing/submit"
+    last_drawings = Repo.all(
+      from d in Drawing,
+      select: %{id: d.id, name: d.name},
+      limit: 10,
+      order_by: [desc: d.inserted_at]
+    )
+
+    render conn, "index.html", drawings: last_drawings
   end
 end
